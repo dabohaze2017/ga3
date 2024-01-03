f355349739ed649b4f5b5b4b32a81207042ab26a
ohara <dabohaze2017+github@gmail.com>
2024-01-03 10:12:47 +0900

    test error

--- src/__tests__/hoge.test.ts
+++ src/__tests__/hoge.test.ts
@@ -2,6 +2,6 @@ import { hoge } from "@/functions/hoge";
 
 describe("関数hogeのテスト", () => {
   test("hoge関数の引数に1を渡したら2が返ってくること", () => {
-    expect(hoge(1)).toBe(2);
+    expect(hoge(1)).toBe(3);
   });
 });
